43363529ce3eda0e0e451a5adc06edf73ad6d424
KiwiDev808 <lucasbacela@hotmail.com>
2021-08-24 21:09:15 -0300

    style: changed homepage title and subtitle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,22 @@
-import Layout from "../components/Layout";
-import BasicMeta from "../components/meta/BasicMeta";
-import OpenGraphMeta from "../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../components/meta/TwitterCardMeta";
-import { SocialList } from "../components/SocialList";
+import Layout from '../components/Layout'
+import BasicMeta from '../components/meta/BasicMeta'
+import OpenGraphMeta from '../components/meta/OpenGraphMeta'
+import TwitterCardMeta from '../components/meta/TwitterCardMeta'
+import { SocialList } from '../components/SocialList'
 
 export default function Index() {
   return (
     <Layout>
-      <BasicMeta url={"/"} />
-      <OpenGraphMeta url={"/"} />
-      <TwitterCardMeta url={"/"} />
+      <BasicMeta url={'/'} />
+      <OpenGraphMeta url={'/'} />
+      <TwitterCardMeta url={'/'} />
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Olá, meu nome é Lucas<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@lucas-b</span>
+          <h2>Esse é um blog de estudos sobre programação.</h2>
           <SocialList />
         </div>
       </div>
@@ -58,5 +58,5 @@ export default function Index() {
         }
       `}</style>
     </Layout>
-  );
+  )
 }
